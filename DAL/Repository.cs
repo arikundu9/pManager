130fbff963b65f221360be411289f77c9e6b03da
Arijit Kundu <arikundu9@gmail.com>
2023-12-27 10:53:13 +0530

    Update Repository.cs

--- DAL/Repository.cs
+++ DAL/Repository.cs
@@ -7,34 +7,34 @@ namespace pMan.DAL
 {
     public abstract class Repository<T, Tcontext> : IRepository<T> where T : class where Tcontext : DbContext
     {
-        protected readonly Tcontext ifmsDbContext = null;
+        protected readonly Tcontext DbContext;
 
         public Repository(Tcontext context)
         {
-            this.ifmsDbContext = context;
+            DbContext = context;
         }
 
 
         public bool IsTransactionRunning()
         {
-            return this.ifmsDbContext.Database.CurrentTransaction == null ? false : true;
+            return DbContext.Database.CurrentTransaction == null ? false : true;
         }
         private IDbContextTransaction BeginTran()
         {
-            return this.ifmsDbContext.Database.BeginTransaction();
+            return DbContext.Database.BeginTransaction();
         }
 
 
 
         public IExecutionStrategy GetExecutionStrategy()
         {
-            return this.ifmsDbContext.Database.CreateExecutionStrategy();
+            return DbContext.Database.CreateExecutionStrategy();
         }
 
 
         public IQueryable<T> GetAllByCondition(Expression<Func<T, bool>> condition)
         {
-            IQueryable<T> result = this.ifmsDbContext.Set<T>();
+            IQueryable<T> result = DbContext.Set<T>();
             if (condition != null)
             {
                 result = result.Where(condition);
@@ -45,7 +45,7 @@ namespace pMan.DAL
 
         public async Task<ICollection<T>> GetAllByConditionAsync(Expression<Func<T, bool>> condition)
         {
-            IQueryable<T> result = this.ifmsDbContext.Set<T>();
+            IQueryable<T> result = DbContext.Set<T>();
             if (condition != null)
             {
                 result = result.Where(condition);
@@ -56,23 +56,23 @@ namespace pMan.DAL
 
         public IQueryable<T> GetAll()
         {
-            IQueryable<T> result = this.ifmsDbContext.Set<T>();
+            IQueryable<T> result = DbContext.Set<T>();
             return result;
         }
 
         public async Task<ICollection<T>> GetAllAsync()
         {
-            IQueryable<T> result = this.ifmsDbContext.Set<T>();
+            IQueryable<T> result = DbContext.Set<T>();
             return await result.ToListAsync();
         }
 
         public T GetSingle(Expression<Func<T, bool>> condition)
         {
-            return this.ifmsDbContext.Set<T>().Where(condition).FirstOrDefault();
+            return DbContext.Set<T>().Where(condition).FirstOrDefault();
         }
         public async Task<T> GetSingleAysnc(Expression<Func<T, bool>> condition)
         {
-            var retValue = await this.ifmsDbContext.Set<T>().Where(condition).SingleOrDefaultAsync();
+            var retValue = await DbContext.Set<T>().Where(condition).SingleOrDefaultAsync();
 
             return retValue;
         }
@@ -80,26 +80,26 @@ namespace pMan.DAL
 
         public bool Add(T entity)
         {
-            this.ifmsDbContext.Set<T>().Add(entity);
+            DbContext.Set<T>().Add(entity);
             return true;
         }
 
         public bool Update(T entity)
         {
-            this.ifmsDbContext.Entry(entity).State = EntityState.Modified;
+            DbContext.Entry(entity).State = EntityState.Modified;
             return true;
         }
 
         public bool Delete(T entity)
         {
-            this.ifmsDbContext.Set<T>().Remove(entity);
+            DbContext.Set<T>().Remove(entity);
             return true;
         }
 
 
         public void SaveChangesManaged()
         {
-            this.ifmsDbContext.SaveChanges();
+            DbContext.SaveChanges();
         }
 
     }
